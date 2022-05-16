44694eddb2dca6a9dbe2f45777767fbce3937492
Vladyslav Dmytrenko <v.dmitrenko1998@gmail.com>
2022-05-16 23:30:11 +0300

    added movie page and css styles

--- src/components/movies/MovieItem/MovieItem.js
+++ src/components/movies/MovieItem/MovieItem.js
@@ -1,21 +1,22 @@
 import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import useHttp from "../../../hooks/use-http";
 
+import classes from "./MovieItem.module.css";
+
 const MovieItem = () => {
   const [movieItem, setMovieItem] = useState();
-  const params = useParams();
-
+  const loginStatus = useSelector((state) => state.login);
   const { sendRequest: fetchMovies } = useHttp();
+
+  const params = useParams();
 
   useEffect(() => {
     const transformMovies = (movies) => {
       const { items } = movies;
-      const movie = items.find((item) => {
-        if (item.id === params.movieId) {
-          return item;
-        } 
-      });
+      // eslint-disable-next-line
+      const movie = items.find((item) => item.id === params.movieId);
       setMovieItem(movie);
     };
     fetchMovies(
@@ -23,15 +24,53 @@ const MovieItem = () => {
       transformMovies
     );
   }, [fetchMovies, params.movieId]);
-  return (
-    <div>
-      <h1>{movieItem.title}</h1>
-			<div>
-				<img src={movieItem.image} alt='#' />
-				<p>{movieItem.plot}</p>
-			</div>
-    </div>
-  );
+
+  let content = movieItem ? (
+    <>
+      <div className={classes["movie-img"]}>
+        <img src={movieItem.image} alt="#" />
+        {loginStatus.isLogin && (
+          <div className={classes["movie-actions"]}>
+            <button>Add to favourite</button>
+            <button>Add to waitlist</button>
+            <button>Write review</button>
+          </div>
+        )}
+      </div>
+      <div className={classes["movie-content"]}>
+        <h1>{movieItem.fullTitle}</h1>
+        <p>
+          <span>Genres: </span>
+          {movieItem.genres}
+        </p>
+        <p>
+          <span>Release Date: </span>
+          {movieItem.releaseState}, {movieItem.year}{" "}
+        </p>
+        <p>
+          <span>Directors: </span>
+          {movieItem.directors}
+        </p>
+        <p>
+          <span>Stars: </span>
+          {movieItem.stars}
+        </p>
+        <p>
+          <span>Content Rating: </span>
+          {movieItem.contentRating}
+        </p>
+        <p>
+          <span>Runtime: </span>
+          {movieItem.runtimeStr}
+        </p>
+        <p>
+          <span>Description: </span>
+          {movieItem.plot}
+        </p>
+      </div>
+    </>
+  ) : null;
+  return <div className={classes.movie}>{content}</div>;
 };
 
 export default MovieItem;
